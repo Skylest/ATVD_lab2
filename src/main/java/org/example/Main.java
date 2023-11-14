649ba417923935518b894b08d6ad6b2085501048
Богдан Ященко <bogdan.yashchenko13@gmail.com>
2023-11-15 00:56:40 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+      /*  selenium_test_ntu testNtu = new selenium_test_ntu();
+        testNtu.setUp();
+        testNtu.precondition();
+        testNtu.testHeaderExist();
+        testNtu.testClickOnForStudent();
+        testNtu.testSearchFieldOnForStudentPage();
+        testNtu.testSlider();
+        testNtu.tearDown();
+
+        selenium_test_rozetka testRozetka = new selenium_test_rozetka();
+        testRozetka.setUp();
+        testRozetka.precondition();
+        testRozetka.testClick();
+        testRozetka.testSearchFieldOnForStudentPage();
+        testRozetka.testSlider();
+        testRozetka.tearDown();*/
+    }
+}
